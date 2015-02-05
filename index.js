8c5f97faa603868752d25457a270b1b8f5c65e0c
Marcos Vieira <mvieira@rico.com.vc>
2015-02-05 15:04:15 -0200

    add exports

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-var artist = process.argv[2] && process.argv[2];
-var song = process.argv[3] && process.argv[3];
-if (!artist || !song) {
-  console.log("Usage: node lyrics_scrape.js [artist] [track]");
-  process.exit(1);
-}
 var sources = require('./sources');
 var wikia = sources.wikia;
 var letras = sources.letras;
 var vagalume = sources.vagalume;
 var darkLyrics = sources.darkLyrics;
 
-wikia.get(artist, song)
-  .fail(function () {
-    console.log('dark');
-    return darkLyrics.get(artist, song);
-  })
-  .fail(function () {
-    console.log('letras');
-    return letras.get(artist, song);
-  })
-  .fail(function () {
-    console.log('vaga');
-    return vagalume.get(artist, song);
-  })
-  .then(callback)
-  .fail(callback);
+var artist = process.argv[2] && process.argv[2];
+var song = process.argv[3] && process.argv[3];
+if (process.argv.length >= 3 && artist && song) {
+  get(artist, song)
+    .then(callback)
+    .fail(callback);
 
-function callback(obj) {
-  console.log(obj, 'eita');
+  function callback(obj) {
+    console.log(obj);
+  }
 }
+
+function get(artist, song) {
+  return wikia.get(artist, song)
+    .fail(function () {
+      return darkLyrics.get(artist, song);
+    })
+    .fail(function () {
+      return letras.get(artist, song);
+    })
+    .fail(function () {
+      return vagalume.get(artist, song);
+    });
+}
+
+exports.get = get;
